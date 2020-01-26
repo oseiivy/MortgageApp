5ea8f021eb086577e5a611db8f50db2e75c34046
Ivy Osei <oseiivy@my.yorku.ca>
2020-01-26 17:46:28 -0500

    Mortgage app commit

--- app/src/main/java/com/example/mortgageapp/EntryForm.java
+++ app/src/main/java/com/example/mortgageapp/EntryForm.java
@@ -3,12 +3,30 @@ package com.example.mortgageapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MortgageActivity extends AppCompatActivity {
+public class EntryForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mortgage_layout);
+        setContentView(R.layout.activity_entry_form);
+    }
+    public void buttonClicked(View v)
+    {
+        EditText principleView = (EditText) findViewById(R.id.principleBox);
+        String principle = principleView.getText().toString();
+        EditText amortizationView = (EditText) findViewById(R.id.amortizationBox);
+        String amortization = amortizationView.getText().toString();
+        EditText interestView = (EditText) findViewById(R.id.interestBox);
+        String interest = interestView.getText().toString();
+
+        MortgageModel myModel = new MortgageModel(principle, amortization, interest);
+        String myMortgage = myModel.computepayment();
+
+        ((TextView) findViewById(R.id.answer)).setText(myMortgage);
+
     }
 }
